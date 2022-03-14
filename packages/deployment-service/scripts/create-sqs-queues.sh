cb36e834afab78abd5631c7b9e9848b792aa9cdb
Ashleigh Simonelli <ashleighsimonelli@gmail.com>
2022-03-14 13:53:32 +0000

    feat: added scripts for localstack

---
+++ packages/deployment-service/scripts/create-sqs-queues.sh
@@ -0,0 +1,5 @@
+# /bin/shell
+
+echo "Creating SQS queues in local"
+
+awslocal sqs create-queue --queue-name something---
---
+++ packages/deployment-service/scripts/start.sh
@@ -0,0 +1,3 @@
+# /bin/shell
+
+SERVICES=sns,sqs,rds,sts,cloudformation,s3,ssm,iam,apigateway localstack start
